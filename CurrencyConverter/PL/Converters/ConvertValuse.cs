d8f639c7c900abea4a7cd4919c563c48127c1c66
nissy34 <33037628+nissy34@users.noreply.github.com>
2018-03-13 18:12:20 +0200

    ערע

--- CurrencyConverter/PL/Converters/ConvertValuse.cs
+++ CurrencyConverter/PL/Converters/ConvertValuse.cs
@@ -17,25 +17,36 @@ namespace PL.Converters
             Currency source, target;
             source = values[0] as Currency;
             target = values[1] as Currency;
-            String factor;
-            factor = values[2] as String;
-
+            //String factor;
+            //factor = values[2] as String;
+            double dfactor = values[2]==null?0:(double)values[2];
             if (source != null && target != null)
             {
-                double result = Math.Round((source.Value / target.Value), 3);
-                double dfactor;
-                if (!String.IsNullOrWhiteSpace(factor) && 
-                    double.TryParse(factor, out dfactor)&&
-                    dfactor>0)
-                 
-                    return (result * dfactor).ToString();
+                double result= (source.Value / target.Value);
+                if(result<0.0000001)
+                    result = Math.Round(result, 10);
+                else
+                    result = Math.Round(result, 5);
+                //if (!(source.IssuedCountryCode == "BTC"))
+                //    result= Math.Round((source.Value / target.Value), 3);
+                //else
+                // result = Math.Round((source.Value / target.Value),10);
+
+                //if (!String.IsNullOrWhiteSpace(factor) && 
+                //    double.TryParse(factor, out dfactor)&&
+                //    dfactor>0)
+
+                if (dfactor> 0)
+                    return (result * (double)values[2]).ToString();
                 
                 else
                     return result.ToString();
             }
 
             if (source != null)
-                return Math.Round(source.Value,3).ToString();
+                if (source.Value < 0.0000001)
+                    return Math.Round(source.Value, 7);
+                else return source.Value;
 
             return "0";
 
